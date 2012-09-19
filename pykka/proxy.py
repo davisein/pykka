324a27f8bec4205b6a0999883fa013bb4a84e09e
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-09-19 02:44:00 +0200

    Imported Upstream version 0.16

--- pykka/proxy.py
+++ pykka/proxy.py
@@ -1,3 +1,6 @@
+import collections as _collections
+import sys as _sys
+
 from pykka import ActorDeadError as _ActorDeadError
 
 
@@ -39,6 +42,39 @@ class ActorProxy(object):
 
         actor_proxy.method_with_side_effect().get()
 
+    An actor can use a proxy to itself to schedule work for itself. The
+    scheduled work will only be done after the current message and all messages
+    already in the inbox are processed.
+
+    For example, if an actor can split a time consuming task into multiple
+    parts, and after completing each part can ask itself to start on the next
+    part using proxied calls or messages to itself, it can react faster to
+    other incoming messages as they will be interleaved with the parts of the
+    time consuming task. This is especially useful for being able to stop the
+    actor in the middle of a time consuming task.
+
+    To create a proxy to yourself, use the actor's :attr:`actor_ref
+    <pykka.actor.Actor.actor_ref>` attribute::
+
+        proxy_to_myself_in_the_future = self.actor_ref.proxy()
+
+    If you create a proxy in your actor's constructor or :meth:`on_start
+    <pykka.actor.Actor.on_start>` method, you can create a nice API for
+    deferring work to yourself in the future::
+
+        def __init__(self):
+            ...
+            self.in_future = self.actor_ref.proxy()
+            ...
+
+        def do_work(self):
+            ...
+            self.in_future.do_more_work()
+            ...
+
+        def do_more_work(self):
+            ...
+
     An example of :class:`ActorProxy` usage:
 
     .. literalinclude:: ../examples/counter.py
@@ -56,22 +92,62 @@ class ActorProxy(object):
         if not actor_ref.is_alive():
             raise _ActorDeadError('%s not found' % actor_ref)
         self.actor_ref = actor_ref
+        # pylint: disable = W0212
+        self._actor = actor_ref._actor
+        # pylint: enable = W0212
         self._attr_path = attr_path or tuple()
-        self._known_attrs = None
+        self._known_attrs = self._get_attributes()
         self._actor_proxies = {}
         self._callable_proxies = {}
 
-    def _update_attrs(self):
-        self._known_attrs = self.actor_ref.ask(
-            {'command': 'pykka_get_attributes'})
+    def _get_attributes(self):
+        """Gathers actor attributes needed to proxy the actor"""
+        result = {}
+        attr_paths_to_visit = [[attr_name] for attr_name in dir(self._actor)]
+        while attr_paths_to_visit:
+            attr_path = attr_paths_to_visit.pop(0)
+            if self._is_exposable_attribute(attr_path[-1]):
+                # pylint: disable = W0212
+                attr = self._actor._get_attribute_from_path(attr_path)
+                # pylint: enable = W0212
+                result[tuple(attr_path)] = {
+                    'callable': self._is_callable_attribute(attr),
+                    'traversable': self._is_traversable_attribute(attr),
+                }
+                if self._is_traversable_attribute(attr):
+                    for attr_name in dir(attr):
+                        attr_paths_to_visit.append(attr_path + [attr_name])
+        return result
+
+    def _is_exposable_attribute(self, attr_name):
+        """
+        Returns true for any attribute name that may be exposed through
+        :class:`ActorProxy`.
+        """
+        return not attr_name.startswith('_')
+
+    def _is_callable_attribute(self, attr):
+        """Returns true for any attribute that is callable."""
+        # isinstance(attr, collections.Callable), as recommended by 2to3, does
+        # not work on CPython 2.6.4 if the attribute is an Queue.Queue, but
+        # works on 2.6.6.
+        if _sys.version_info < (3,):
+            return callable(attr)
+        else:
+            return isinstance(attr, _collections.Callable)
+
+    def _is_traversable_attribute(self, attr):
+        """
+        Returns true for any attribute that may be traversed from another
+        actor through a proxy.
+        """
+        return hasattr(attr, 'pykka_traversable')
 
     def __repr__(self):
         return '<ActorProxy for %s, attr_path=%s>' % (self.actor_ref,
             self._attr_path)
 
     def __dir__(self):
-        if self._known_attrs is None:
-            self._update_attrs()
         result = ['__class__']
         result += list(self.__class__.__dict__.keys())
         result += list(self.__dict__.keys())
@@ -82,8 +158,8 @@ class ActorProxy(object):
     def __getattr__(self, name):
         """Get a field or callable from the actor."""
         attr_path = self._attr_path + (name,)
-        if self._known_attrs is None or attr_path not in self._known_attrs:
-            self._update_attrs()
+        if attr_path not in self._known_attrs:
+            self._known_attrs = self._get_attributes()
         attr_info = self._known_attrs.get(attr_path)
         if attr_info is None:
             raise AttributeError('%s has no attribute "%s"' % (self, name))
--- tests/method_call_test.py
+++ tests/method_call_test.py
@@ -1,8 +1,12 @@
-import os
-import sys
 import unittest
 
 from pykka.actor import ThreadingActor
+
+try:
+    from pykka.gevent import GeventActor
+    HAS_GEVENT = True
+except ImportError:
+    HAS_GEVENT = False
 
 
 class ActorWithMethods(object):
@@ -17,20 +21,24 @@ class ActorWithMethods(object):
     def raise_keyboard_interrupt(self):
         raise KeyboardInterrupt
 
+    def talk_with_self(self):
+        return self.actor_ref.proxy().functional_hello('from the future')
+
 
 class ActorExtendableAtRuntime(object):
     def add_method(self, name):
         setattr(self, name, lambda: 'returned by ' + name)
 
+    def use_foo_through_self_proxy(self):
+        return self.actor_ref.proxy().foo()
 
-class MethodCallTest(object):
+
+class StaticMethodCallTest(object):
     def setUp(self):
         self.proxy = self.ActorWithMethods.start().proxy()
-        self.proxy_extendable = self.ActorExtendableAtRuntime().start().proxy()
 
     def tearDown(self):
         self.proxy.stop()
-        self.proxy_extendable.stop()
 
     def test_functional_method_call_returns_correct_value(self):
         self.assertEqual('Hello, world!',
@@ -52,25 +60,54 @@ class MethodCallTest(object):
             self.assert_(result.startswith('<ActorProxy for ActorWithMethods'))
             self.assert_(result.endswith('has no attribute "unknown_method"'))
 
+    def test_can_proxy_itself_for_offloading_work_to_the_future(self):
+        outer_future = self.proxy.talk_with_self()
+        inner_future = outer_future.get(timeout=1)
+        result = inner_future.get(timeout=1)
+        self.assertEqual('Hello, from the future!', result)
+
+
+class DynamicMethodCallTest(object):
+    def setUp(self):
+        self.proxy = self.ActorExtendableAtRuntime.start().proxy()
+
+    def tearDown(self):
+        self.proxy.stop()
+
     def test_can_call_method_that_was_added_at_runtime(self):
-        self.proxy_extendable.add_method('foo')
-        self.assertEqual('returned by foo', self.proxy_extendable.foo().get())
+        # We need to .get() after .add_method() to be sure that the method has
+        # been added before we try to use it through the proxy.
+        self.proxy.add_method('foo').get()
+        self.assertEqual('returned by foo', self.proxy.foo().get())
+
+    def test_can_proxy_itself_and_use_attrs_added_at_runtime(self):
+        # We don't need to .get() after .add_method() here, because the actor
+        # will process the .add_method() call before processing the
+        # .use_foo_through_self_proxy() call, which again will use the new
+        # method, .foo().
+        self.proxy.add_method('foo')
+        outer_future = self.proxy.use_foo_through_self_proxy()
+        inner_future = outer_future.get(timeout=1)
+        result = inner_future.get(timeout=1)
+        self.assertEqual('returned by foo', result)
 
 
-class ThreadingMethodCallTest(MethodCallTest, unittest.TestCase):
+class ThreadingStaticMethodCallTest(StaticMethodCallTest, unittest.TestCase):
     class ActorWithMethods(ActorWithMethods, ThreadingActor):
         pass
 
+
+class ThreadingDynamicMethodCallTest(DynamicMethodCallTest, unittest.TestCase):
     class ActorExtendableAtRuntime(ActorExtendableAtRuntime, ThreadingActor):
         pass
 
 
-if sys.version_info < (3,) and 'TRAVIS' not in os.environ:
-    from pykka.gevent import GeventActor
-
-    class GeventMethodCallTest(MethodCallTest, unittest.TestCase):
+if HAS_GEVENT:
+    class GeventStaticMethodCallTest(StaticMethodCallTest, unittest.TestCase):
         class ActorWithMethods(ActorWithMethods, GeventActor):
             pass
 
+    class GeventDynamicMethodCallTest(DynamicMethodCallTest,
+            unittest.TestCase):
         class ActorExtendableAtRuntime(ActorExtendableAtRuntime, GeventActor):
             pass

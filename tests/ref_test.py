743bf0031ad4f6419b2f7dcf412444087410adb6
Stein Magnus Jodal <stein.magnus@jodal.no>
2012-10-26 22:11:21 +0200

    Imported Upstream version 1.0.0

--- tests/ref_test.py
+++ tests/ref_test.py
@@ -15,6 +15,7 @@ except ImportError:
 
 class AnActor(object):
     def __init__(self, received_message):
+        super(AnActor, self).__init__()
         self.received_message = received_message
 
     def on_receive(self, message):
@@ -72,26 +73,13 @@ class RefTest(object):
 
     def test_tell_delivers_message_to_actors_custom_on_receive(self):
         self.ref.tell({'command': 'a custom message'})
-        self.assertEqual({'command': 'a custom message'},
-            self.received_message.get())
+        self.assertEqual(
+            {'command': 'a custom message'}, self.received_message.get())
 
     def test_tell_fails_if_actor_is_stopped(self):
         self.ref.stop()
         try:
             self.ref.tell({'command': 'a custom message'})
-            self.fail('Should raise ActorDeadError')
-        except ActorDeadError as exception:
-            self.assertEqual('%s not found' % self.ref, str(exception))
-
-    def test_send_one_way_delivers_message_to_actors_custom_on_receive(self):
-        self.ref.send_one_way({'command': 'a custom message'})
-        self.assertEqual({'command': 'a custom message'},
-            self.received_message.get())
-
-    def test_send_one_way_fails_if_actor_is_stopped(self):
-        self.ref.stop()
-        try:
-            self.ref.send_one_way({'command': 'a custom message'})
             self.fail('Should raise ActorDeadError')
         except ActorDeadError as exception:
             self.assertEqual('%s not found' % self.ref, str(exception))
@@ -115,29 +103,6 @@ class RefTest(object):
         self.ref.stop()
         try:
             self.ref.ask({'command': 'ping'})
-            self.fail('Should raise ActorDeadError')
-        except ActorDeadError as exception:
-            self.assertEqual('%s not found' % self.ref, str(exception))
-
-    def test_send_request_reply_blocks_until_response_arrives(self):
-        result = self.ref.send_request_reply({'command': 'ping'})
-        self.assertEqual('pong', result)
-
-    def test_send_request_reply_can_timeout_if_blocked_too_long(self):
-        try:
-            self.ref.send_request_reply({'command': 'ping'}, timeout=0)
-            self.fail('Should raise Timeout exception')
-        except Timeout:
-            pass
-
-    def test_send_request_reply_can_return_future_instead_of_blocking(self):
-        future = self.ref.send_request_reply({'command': 'ping'}, block=False)
-        self.assertEqual('pong', future.get())
-
-    def test_send_request_reply_fails_if_actor_is_stopped(self):
-        self.ref.stop()
-        try:
-            self.ref.send_request_reply({'command': 'ping'})
             self.fail('Should raise ActorDeadError')
         except ActorDeadError as exception:
             self.assertEqual('%s not found' % self.ref, str(exception))
